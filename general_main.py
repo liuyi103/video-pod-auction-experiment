0dadc38c7f7f591e3f845b37d01c410baf6ce8bf
liuyi103 <liuyicheng1991@hotmail.com>
2015-12-11 22:34:30 +0800

    cplex version

--- general_main.py
+++ general_main.py
@@ -7,23 +7,18 @@ Created on Sat Aug 08 22:31:09 2015
 from basic_functions import *
 
 if __name__ == '__main__':
-    max_duration = 100
+    max_duration = 200
     ads = data_sampler(100)
     winners = set(select_winners(ads, max_duration))
     losers = set(ads) - winners
     bar = [i[1] for i in get_coutour(losers, max_duration, True)]
+    
+    ## Using pulp
     constraints = {}
-
-    print len(winners)
-    print bar
-    for winner in winners:
-        print winner.bid, winner.duration
-
     for winner in winners:
         constraints[(winner,)] = bar[winner['duration']]
+    
     while True:
-        print len(constraints)
-        raw_input()
         get_solution(winners, constraints)
         lower_coutour = get_coutour(winners, max_duration, False)
         finish = True
@@ -32,10 +27,40 @@ if __name__ == '__main__':
             if len(ads) == 0:
                 continue
             if min_value < bar[k] - 1e-5:
-                print min_value, bar[k]
                 constraints[tuple(ads)] = bar[k]
                 finish = False
         if finish:
             break
-    for winner in winners:
-        print winner.price
+    print sum([winner.price for winner in winners])
+    
+    # using cplex directly
+    from cplex import Cplex
+    winners = list(winners)
+    id2num = {id(winner):k for k, winner in enumerate(winners)}
+    prob = Cplex()
+    lb = [bar[winner.duration] for winner in winners]
+    ub = [winner.bid for winner in winners]
+    prob.objective.set_sense(prob.objective.sense.minimize)
+    obj = [1] * len(winners)
+    prob.variables.add(obj, lb, ub)
+    while True:
+        prob.solve()
+        prices = prob.solution.get_values()
+        for i in range(len(winners)):
+            winners[i].price = prices[i]
+        lower_coutour = get_coutour(winners, max_duration, False)
+        finish = True
+        for k, tmp in enumerate(lower_coutour):
+            ads, min_value = tmp
+            if len(ads) == 0:
+                continue
+            if min_value < bar[k] - 1e-5:
+                prob.linear_constraints.add([[[id2num[id(ad)] for ad in ads],[1] * len(ads)]],\
+                ['G'], [bar[k]])
+                finish = False
+        if finish:
+            break
+    print sum(prob.solution.get_values())
+    
+    
+    
