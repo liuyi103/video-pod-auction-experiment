98fb4ff146b33736734375ae82811b7614b35d07
liuyi103 <liuyicheng1991@hotmail.com>
2015-12-12 13:55:56 +0800

    monotone constrant

--- general_main.py
+++ general_main.py
@@ -7,35 +7,35 @@ Created on Sat Aug 08 22:31:09 2015
 from basic_functions import *
 
 if __name__ == '__main__':
-    max_duration = 200
-    ads = data_sampler(100)
+    max_duration = 500
+    ads = data_sampler(200)
     winners = set(select_winners(ads, max_duration))
     losers = set(ads) - winners
     bar = [i[1] for i in get_coutour(losers, max_duration, True)]
     
     ## Using pulp
-    constraints = {}
-    for winner in winners:
-        constraints[(winner,)] = bar[winner['duration']]
-    
-    while True:
-        get_solution(winners, constraints)
-        lower_coutour = get_coutour(winners, max_duration, False)
-        finish = True
-        for k, tmp in enumerate(lower_coutour):
-            ads, min_value = tmp
-            if len(ads) == 0:
-                continue
-            if min_value < bar[k] - 1e-5:
-                constraints[tuple(ads)] = bar[k]
-                finish = False
-        if finish:
-            break
-    print sum([winner.price for winner in winners])
-    
+#    constraints = {}
+#    for winner in winners:
+#        constraints[(winner,)] = bar[winner['duration']]
+#    
+#    while True:
+#        get_solution(winners, constraints)
+#        lower_coutour = get_coutour(winners, max_duration, False)
+#        finish = True
+#        for k, tmp in enumerate(lower_coutour):
+#            ads, min_value = tmp
+#            if len(ads) == 0:
+#                continue
+#            if min_value < bar[k] - 1e-5:
+#                constraints[tuple(ads)] = bar[k]
+#                finish = False
+#        if finish:
+#            break
+#    print sum([winner.price for winner in winners])
+#    
     # using cplex directly
     from cplex import Cplex
-    winners = list(winners)
+    winners = sorted(winners, key = lambda winner: (winner.duration, winner.bid))
     id2num = {id(winner):k for k, winner in enumerate(winners)}
     prob = Cplex()
     lb = [bar[winner.duration] for winner in winners]
@@ -43,6 +43,22 @@ if __name__ == '__main__':
     prob.objective.set_sense(prob.objective.sense.minimize)
     obj = [1] * len(winners)
     prob.variables.add(obj, lb, ub)
+    
+    # monotone requirement
+    for i, x in enumerate(winners):
+        ceil = 1e9
+        for k, y in enumerate(winners[i+1:]):
+            j = i + 1 + k
+            if y.bid == x.bid:
+                if x.duration == y.duration:
+                    prob.linear_constraints.add([[[i, j], [1,-1]]], ['E'], [0])
+                else:
+                    prob.linear_constraints.add([[[i, j],[1,-1]]],['L'],[0])
+                break
+            if y.bid > x.bid and y.bid < ceil:
+                ceil = y.bid
+                prob.linear_constraints.add([[[i,j],[1,-1]]],['L'],[0])
+    
     while True:
         prob.solve()
         prices = prob.solution.get_values()
@@ -60,7 +76,7 @@ if __name__ == '__main__':
                 finish = False
         if finish:
             break
-    print sum(prob.solution.get_values())
+    print sum(prob.solution.get_values()), len(winners)
     
     
     
