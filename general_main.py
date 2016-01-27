583b3d043e39c204cb650ca8d3a0e3bbf9f01555
liuyi103 <liuyicheng1991@hotmail.com>
2016-01-27 23:00:41 +0800

    bug still

--- general_main.py
+++ general_main.py
@@ -30,26 +30,25 @@ def solveit(max_duration, ads):
     prob.variables.add(obj, lb, ub)
 
     # monotone requirement (optional)
-    for i, x in enumerate(winners):
-        ceil = 1e9
-        for k, y in enumerate(winners[i+1:]):
-            j = i + 1 + k
-            if y.bid == x.bid:
-                if x.duration == y.duration:
-                    prob.linear_constraints.add([[[i, j], [1,-1]]], ['E'], [0])
-                else:
-                    prob.linear_constraints.add([[[i, j],[1,-1]]],['L'],[0])
-                break
-            if y.bid > x.bid and y.bid < ceil:
-                ceil = y.bid
-                prob.linear_constraints.add([[[i,j],[1,-1]]],['L'],[0])
+    # for i, x in enumerate(winners):
+    #     ceil = 1e9
+    #     for k, y in enumerate(winners[i+1:]):
+    #         j = i + 1 + k
+    #         if y.bid == x.bid:
+    #             if x.duration == y.duration:
+    #                 prob.linear_constraints.add([[[i, j], [1,-1]]], ['E'], [0])
+    #             else:
+    #                 prob.linear_constraints.add([[[i, j],[1,-1]]],['L'],[0])
+    #             break
+    #         if y.bid > x.bid and y.bid < ceil:
+    #             ceil = y.bid
+    #             prob.linear_constraints.add([[[i,j],[1,-1]]],['L'],[0])
 
     n_iteration = 0
     while True:
         n_iteration += 1
         prob.solve()
         prices = prob.solution.get_values()
-        print len(prices), len(winners)
         for i, winner in enumerate(winners):
             winner.price = prices[i]
         lower_coutour = get_coutour(winners, max_duration, False)
@@ -58,21 +57,83 @@ def solveit(max_duration, ads):
             ads, min_value = tmp
             if len(ads) == 0:
                 continue
-            if min_value < bar[k] - 1e-5:
-                prob.linear_constraints.add([[[id2num[id(ad)] for ad in ads],[1] * len(ads)]],\
+            if min_value < bar[k] - 1e-4:
+                prob.linear_constraints.add([[[id2num[id(ad)] for ad in ads], [1] * len(ads)]],\
                     ['G'], [bar[k]])
                 finish = False
         if finish:
             break
     return sum(prob.solution.get_values()), len(winners), n_iteration
 
+def solveit2(max_duration, ads):
+    '''
+    add only 1 constraint each iteration
+    :param max_duration:
+    :param ads:
+    :return:
+    '''
+    # get the winners
+    winners, losers = select_winners(ads, max_duration)
+    llosers = list(losers)
+    # add a fake winner if needed.
+    total_dur = sum([winner.duration for winner in winners])
+    if total_dur < max_duration:
+        winners.add(Ad(bid = 0, duration =max_duration - total_dur))
+
+    # get the upper contour for the losers
+    bar = [i[1] for i in get_coutour(losers, max_duration, True)]
+
+    winners = sorted(winners, key = lambda winner: (winner.duration, winner.bid))
+    id2num = {id(winner):k for k, winner in enumerate(winners)}
+    prob = Cplex()
+    lb = [bar[winner.duration] for winner in winners]
+    ub = [winner.bid for winner in winners]
+    prob.objective.set_sense(prob.objective.sense.minimize)
+    obj = [1] * len(winners)
+    prob.variables.add(obj, lb, ub)
+
+    # monotone requirement (optional)
+    # for i, x in enumerate(winners):
+    #     ceil = 1e9
+    #     for k, y in enumerate(winners[i+1:]):
+    #         j = i + 1 + k
+    #         if y.bid == x.bid:
+    #             if x.duration == y.duration:
+    #                 prob.linear_constraints.add([[[i, j], [1,-1]]], ['E'], [0])
+    #             else:
+    #                 prob.linear_constraints.add([[[i, j],[1,-1]]],['L'],[0])
+    #             break
+    #         if y.bid > x.bid and y.bid < ceil:
+    #             ceil = y.bid
+    #             prob.linear_constraints.add([[[i,j],[1,-1]]],['L'],[0])
+
+    n_iteration = 0
+    while True:
+        n_iteration += 1
+        prob.solve()
+        prices = prob.solution.get_values()
+        fake_winners = [Ad(bid=prices[k], duration=winner.duration) for k,winner in enumerate(winners)]
+        tmp_winners, _ = select_winners(fake_winners+llosers, max_duration)
+        sum1 = sum([winner['bid'] for winner in tmp_winners])
+        sum2 = sum([winner['bid'] for winner in fake_winners])
+        if sum1 > sum2 + 1e-5:
+            c = sum1 - sum2
+            ind = []
+            for k, winner in enumerate(fake_winners):
+                if winner not in tmp_winners:
+                    ind.append(k)
+                    c += winner['price']
+            prob.linear_constraints.add([[ind, [1]*len(ind)]], 'G', [c])
+        else:
+            break
+    return sum(prob.solution.get_values()), len(winners), n_iteration
 
 if __name__ == '__main__':
     n_iters = []
     times = []
-    for i in range(10):
-        max_duration = 200
-        ads = data_sampler(50)
+    for i in range(100):
+        max_duration = 500
+        ads = data_sampler(200)
         st_time = time.time()
         revenue, n_winners, n_iter = solveit(max_duration, ads)
         en_time = time.time()
@@ -80,3 +141,4 @@ if __name__ == '__main__':
         times.append(en_time - st_time)
         print revenue, n_winners
     print n_iters, times
+
